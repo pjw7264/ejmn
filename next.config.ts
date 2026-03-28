3ba922638c031ac60b6cb42c9d3f059a029ff7ea
pjw7264 <pjw7264@users.noreply.github.com>
2026-03-29 00:17:17 +0900

    fix(dev): 개발 서버 허용 오리진 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  allowedDevOrigins: ["172.20.10.2"],
 };
 
 export default nextConfig;
